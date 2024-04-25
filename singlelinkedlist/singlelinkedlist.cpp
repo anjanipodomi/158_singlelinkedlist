72dbf436925d74b47f5364e1bd586f0265290d0c
anjanipodomi <anjanidihapsari@gmail.com>
2024-04-25 10:15:07 +0700

    menambahkan lopping if listEmpty

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -90,7 +90,12 @@ bool listEmpty() {
 }
 
 void traverse() {
-
+    if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
 }
 
 int main()
