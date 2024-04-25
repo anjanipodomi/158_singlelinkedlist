64939b241ac444637446368e84efb7d6cf7042f9
anjanipodomi <anjanidihapsari@gmail.com>
2024-04-25 10:10:50 +0700

    menambahkan lopping if start=current->next, return true

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -76,6 +76,9 @@ bool deleteNode(int nim) {
     if (serachNode(nim, previous, current) == false)
         return false;
     previous->next = current->next;
+    if (current == START)
+        START = current->next;
+    return true;
 }
 
 int main()
